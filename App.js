88dcb806f96ed39bb667a91324c8d93161e8fc97
Gunhee94 <60492505+Gunhee94@users.noreply.github.com>
2023-01-17 22:40:13 +0900

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+import { useState } from 'react';
+import { ImageViewer } from './components/ImageViewer';
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState("");
+
+  const addPhoto = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      // allowsEditing: true,   // 사진 편집
+      allowMultipleSelection : true,
+      selectionLimit : 20,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result)
+      setSelectedImage(result.assets[0].uri);
+    }
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity style={styles.photoBox} onPress={addPhoto}>
+        <MaterialIcons name="photo-camera" size={120} color="gray" />
+      </TouchableOpacity>
+      <ImageViewer
+          selectedImage={selectedImage}
+        />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  photoBox: {
+    borderWidth : 1,
+    width : "50%",
+    height : "25%",
+    borderColor: "gray",
+    alignItems : "center",
+    justifyContent :"center"
   },
 });
---
+++ components/ImageViewer.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Image, Dimensions, View } from 'react-native';
+
+export const ImageViewer = ({ placeholderImageSource, selectedImage }) => {
+    const WINDOW_WIDTH = Dimensions.get("window").width;
+    const imageSource = selectedImage !== null
+    ? { uri: selectedImage }
+    : placeholderImageSource;
+
+  return (
+    <View style={stylesImage(WINDOW_WIDTH).imageBox}>
+        <Image source={imageSource} style={styles.image} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
+
+const stylesImage = (width) =>
+    StyleSheet.create({
+        imageBox: {
+            padding: 1,
+            width: "50%",
+            height: width / 2,
+        },
+    });
