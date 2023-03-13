abf6d9fae7b2168c6926c65849a8df3feeab6da8
gunhee.lee <gunhee.lee@lgepartner.com>
2023-03-13 17:53:18 +0900

    사진 여러장 업로드 가능

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,28 +1,28 @@
 import { StyleSheet, Image, Dimensions, View } from "react-native";
 
 export const ImageViewer = ({ placeholderImageSource, selectedImage }) => {
-  const WINDOW_WIDTH = Dimensions.get("window").width;
-  const imageSource =
-    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
-  return (
-    <View style={stylesImage(WINDOW_WIDTH).imageBox}>
-      <Image source={imageSource} style={styles.image} />
-    </View>
-  );
+    const WINDOW_WIDTH = Dimensions.get("window").width;
+    const imageSource =
+        selectedImage !== null ? { uri: selectedImage.uri } : placeholderImageSource;
+    return (
+        <View style={stylesImage(WINDOW_WIDTH).imageBox}>
+            <Image source={imageSource} style={styles.image} />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  image: {
-    width: "100%",
-    height: "100%",
-  },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
 });
 
 const stylesImage = (width) =>
-  StyleSheet.create({
-    imageBox: {
-      padding: 1,
-      width: width / 4,
-      height: width / 4,
-    },
-  });
+    StyleSheet.create({
+        imageBox: {
+            padding: 1,
+            width: width / 4,
+            height: width / 4,
+        },
+    });
--- components/Memories.js
+++ components/Memories.js
@@ -5,83 +5,83 @@ import { Animated } from "react-native";
 import useInterval from "./../hooks/useInterval";
 
 export default function Memories({ route }) {
-  const images = route.params;
-  const [selectedImages, setSelectedImages] = useState(images.uri);
-  const [animation, setAnimation] = useState(new Animated.Value(0));
-  const [aniNum, setAniNum] = useState(0);
-  const [image, setImage] = useState(selectedImages[0]);
-  const [aniId, setAniId] = useState("");
-  const [count, setCount] = useState(0);
+    const images = route.params;
 
-  let animationList = [
-    {
-      id: "moveImage",
-      action: Animated.timing(animation, {
-        toValue: 250,
-        duration: 2000,
-        useNativeDriver: true,
-      }),
-    },
-    {
-      id: "fadeInImage",
-      action: Animated.timing(animation, {
-        toValue: 1,
-        duration: 2000,
-        useNativeDriver: true,
-      }),
-    },
-    {
-      id: "scaleImage",
-      action: Animated.timing(animation, {
-        toValue: 1,
-        duration: 2000,
-        useNativeDriver: true,
-      }),
-    },
-  ];
+    const [selectedImages, setSelectedImages] = useState(images);
+    const [animation, setAnimation] = useState(new Animated.Value(0));
+    const [aniNum, setAniNum] = useState(0);
+    const [image, setImage] = useState(selectedImages[0].uri);
+    const [aniId, setAniId] = useState("");
+    const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    animationList.sort(() => Math.random() - 0.5);
+    let animationList = [
+        {
+            id: "moveImage",
+            action: Animated.timing(animation, {
+                toValue: 250,
+                duration: 2000,
+                useNativeDriver: true,
+            }),
+        },
+        {
+            id: "fadeInImage",
+            action: Animated.timing(animation, {
+                toValue: 1,
+                duration: 2000,
+                useNativeDriver: true,
+            }),
+        },
+        {
+            id: "scaleImage",
+            action: Animated.timing(animation, {
+                toValue: 1,
+                duration: 2000,
+                useNativeDriver: true,
+            }),
+        },
+    ];
 
-    animationList[aniNum].action.start;
-    console.log("몇번", animationList[aniNum].action);
-  }, [aniNum]);
+    useEffect(() => {
+        animationList.sort(() => Math.random() - 0.5);
 
-  useInterval(
-    () => {
-      setCount((count) => count + 1);
-      setAniNum((aniNum) => aniNum + 1);
-      setImage(selectedImages[aniNum]);
-      setAniId(animationList[aniNum].id);
-    },
-    selectedImages.length > count ? 2000 : null
-  );
+        animationList[aniNum].action.start;
+    }, [aniNum]);
 
-  console.log("뭐야", count, aniNum, aniId);
-  return (
-    <Animated.View style={stylesAnimation(animation).aniId}>
-      <StatusBar style="auto" hidden />
-      <Image source={{ uri: image }} style={styles.image} />
-    </Animated.View>
-  );
+    useInterval(
+        () => {
+            setCount((count) => count + 1);
+            setAniNum((aniNum) => aniNum + 1);
+            setImage(selectedImages[aniNum].uri);
+            setAniId(animationList[aniNum].id);
+        },
+        selectedImages.length > count ? 2000 : null
+    );
+
+    console.log("뭐야", count, aniNum, aniId);
+    return (
+        <Animated.View style={stylesAnimation(animation).aniId}>
+            <StatusBar style="auto" hidden />
+            <Image source={{ uri: image }} style={styles.image} />
+        </Animated.View>
+    );
 }
 
 const stylesAnimation = (animation) =>
-  StyleSheet.create({
-    moveImage: {
-      transform: [{ translateY: animation }],
-    },
-    fadeInImage: {
-      opacity: animation,
-    },
-    scaleImage: {
-      transform: [{ scale: animation }],
-    },
-  });
+    StyleSheet.create({
+        moveImage: {
+            transform: [{ translateY: animation }],
+        },
+        fadeInImage: {
+            opacity: animation,
+        },
+        scaleImage: {
+            transform: [{ scale: animation }],
+        },
+    });
 
 const styles = StyleSheet.create({
-  image: {
-    width: "100%",
-    height: "100%",
-  },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
 });
